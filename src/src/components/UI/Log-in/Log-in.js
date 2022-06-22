663e146a1a3a42f17440207e671275c2c28915aa
DavidGjurchinoski <davidddmondavid@gmail.com>
2022-06-22 22:52:24 +0200

    Starting forlder structure

---
+++ src/src/components/UI/Log-in/Log-in.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Log-in.css';
+
+const Log-in = () => (
+  <div className="Log-in">
+    Log-in Component
+  </div>
+);
+
+Log-in.propTypes = {};
+
+Log-in.defaultProps = {};
+
+export default Log-in;
