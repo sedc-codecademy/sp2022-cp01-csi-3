4f22191fcd0d230f91d2bb0ea8d8cd0f68a625f9
DavidGjurchinoski <davidddmondavid@gmail.com>
2022-06-28 10:26:08 +0200

    Not found, Routing and Home

--- src/src/App.js
+++ src/src/App.js
@@ -1,25 +1,34 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/UI/Header/Header';
+import Footer from './components/UI/Footer/Footer';
+import { Home } from '@material-ui/icons';
+import Profile from './components/UI/Profile/Profile';
+import SignUp from './components/UI/SignUp/SignUp';
+import LogIn from './components/UI/LogIn/LogIn';
+import About from './components/UI/About/About';
+import TermsOfService from './components/UI/TermsOfService/TermsOfService';
+import NotFound404 from './components/UI/NotFound404/NotFound404';
 
 function App() {
   return (
 	  <div className="App">
 	  	<Header></Header>
-      	<header className="App-header">
-			<h1>	
-				Welcome to Crypto Simulator
-			</h1> 
-        	<img src={logo} className="App-logo" alt="logo" />
-        	<a
-        	  className="App-link"
-        	  href="https://reactjs.org"
-        	  target="_blank"
-        	  rel="noopener noreferrer"
-        	>
-				Link for React DOCS
-        	</a>
-      	</header>
+      	
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/profile' element={<Profile />} />
+				<Route path='/log-in' element={<LogIn />} />
+				<Route path='/sign-up' element={<SignUp />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/terms-of-service' element={<TermsOfService />} />
+				<Route path='*' element={<NotFound404 />} />
+			</Routes>
+		</BrowserRouter>
+
 		<Footer></Footer>
     </div>
   );
---
+++ src/src/components/UI/NotFound404/NotFound404.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './NotFound404.css';
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+
+const NotFound404 = () => (
+	<div className='NotFound404'>
+		<div className='c'>
+			<div className='_404'>404</div>
+				<br />
+			<hr />
+				<br />
+			<div className='_1'>THE PAGE</div>
+				<br />
+			<div className='_2'>WAS NOT FOUND</div>
+				<br />
+				<br />
+			<Button variant="contained">
+				<Link to={'/'}>
+					Home
+				</Link>
+			</Button>
+		</div>
+	</div>
+);
+
+NotFound404.propTypes = {};
+
+NotFound404.defaultProps = {};
+
+export default NotFound404;
