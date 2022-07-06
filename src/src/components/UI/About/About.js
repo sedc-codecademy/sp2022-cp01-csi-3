a97626cd1810a6cf8cda30a3d7e034febe37bf53
David S <davidsumanski20@gmail.com>
2022-07-06 14:51:06 +0200

    commited about

--- src/src/components/UI/About/About.js
+++ src/src/components/UI/About/About.js
@@ -15,47 +15,55 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+const ListComponent = () => { 
+  return ( 
+    <div> 
+      <h1>TEST</h1> 
+    </div> 
+  ); 
+}; 
+
 export default function ResponsiveGrid() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 }} className="Container">
       <Grid className="Grid" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        <Grid className="Box" item xs={2} sm={4} md={4}>
-            <Item><img className="Image" src='https://cdn-icons-png.flaticon.com/512/5341/5341448.png'></img>
-            <h1>How to use crypto</h1>
-            <ul>
-              <li className="ListItem">Secure your crypto</li>
-              <li className="ListItem">Make transactions</li>
-              <li className="ListItem">Advanced crypto use-cases</li>
-            </ul>
-            <Button variant="contained">
-                Learn More →
-            </Button>
+        <Grid  className="Box" item xs={2} sm={4} md={4}>
+            <Item className='Item'><img className="Image" src='https://cdn-icons-png.flaticon.com/512/5341/5341448.png'></img>
+              <h1>How to use crypto</h1>
+              <ul>
+                <li className="ListItem">Secure your crypto</li>
+                <li className="ListItem">Make transactions</li>
+                <li className="ListItem">Advanced crypto use-cases</li>
+              </ul>
+              <Button variant="contained" onClick={ListComponent}>
+                  Learn More →
+              </Button>
             </Item>
           </Grid>
           <Grid className="Box" item xs={2} sm={4} md={4}>
             <Item><img className="Image" src='https://cdn-icons-png.flaticon.com/512/4285/4285667.png'></img>
-            <h1>How to trade crypto</h1>
-            <ul>
-              <li className="ListItem">Understanding risk</li>
-              <li className="ListItem">Investing vs Trading</li>
-              <li className="ListItem">Technical & Fundamental Analysis</li>
-            </ul>
-            <Button variant="contained">
-                Learn More →
-            </Button>
+              <h1>How to trade crypto</h1>
+              <ul>
+                <li className="ListItem">Understanding risk</li>
+                <li className="ListItem">Investing vs Trading</li>
+                <li className="ListItem">Technical & Fundamental Analysis</li>
+              </ul>
+              <Button variant="contained">
+                  Learn More →
+              </Button>
             </Item>
           </Grid>
           <Grid className="Box" item xs={2} sm={4} md={4}>
             <Item><img className="Image" src='https://cdn-icons-png.flaticon.com/512/2685/2685027.png'></img>
-            <h1>How to earn crypto</h1>
-            <ul>
-              <li className="ListItem">Earn by Learning & Working</li>
-              <li className="ListItem">Trading, Defi & Mining</li>
-              <li className="ListItem">Hoddling, DCA & Passive Interest</li>
-            </ul>
-            <Button variant="contained">
-                Learn More →
-            </Button>
+              <h1>How to earn crypto</h1>
+              <ul>
+                <li className="ListItem">Earn by Learning & Working</li>
+                <li className="ListItem">Trading, Defi & Mining</li>
+                <li className="ListItem">Hoddling, DCA & Passive Interest</li>
+              </ul>
+              <Button variant="contained">
+                  Learn More →
+              </Button>
             </Item>
           </Grid>
       </Grid>
